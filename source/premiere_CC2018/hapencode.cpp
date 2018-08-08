afe0a853ee6c967002416654c88d56a3b52f4e72
Greg Bakker <gbakker@gmail.com>
2018-08-08 10:46:52 +0100

    multithreading: thread safety for exporter wrapper

--- source/premiere_CC2018/hapencode.cpp
+++ source/premiere_CC2018/hapencode.cpp
@@ -6,7 +6,8 @@ Exporter::Exporter(
     std::unique_ptr<Codec> codec,
     std::unique_ptr<MovieWriter> writer,
     int64_t nFrames)
-  : codec_(std::move(codec)), writer_(std::move(writer)), nFrames_(nFrames)
+  : codec_(std::move(codec)), writer_(std::move(writer)), nFrames_(nFrames),
+    nextFrameToWrite_(0)
 
 {
 }
@@ -15,17 +16,68 @@ Exporter::~Exporter()
 {
 }
 
-// TODO: thread-safe
 void Exporter::dispatch(int64_t iFrame, const uint8_t* bgra_bottom_left_origin_data, size_t stride) const
 {
-    // convert Premiere frame to HAP frame
-    input_.bgraBottomLeftOrigin = (uint8_t *)bgra_bottom_left_origin_data;
-    input_.stride = stride;
+    std::unique_ptr<ExporterEncodeBuffers> buffers = getEncodeBuffers();
 
-    codec_->encode(input_, scratchpad_, output_);
+    // convert Premiere frame to HAP frame
+    buffers->input.bgraBottomLeftOrigin = (uint8_t *)bgra_bottom_left_origin_data;
+    buffers->input.stride = stride;
+
+    codec_->encode(buffers->input, buffers->scratchpad, buffers->output);
 
     // write it out
-    writer_->writeFrame(&output_.buffer[0], output_.buffer.size());
+    writeInSequence(iFrame, std::move(buffers));
+}
+
+std::unique_ptr<ExporterEncodeBuffers> Exporter::getEncodeBuffers() const
+{
+    std::lock_guard<std::mutex> guard(freeListMutex_);
+    if (!freeList_.empty())
+    {
+        auto ret = std::move(freeList_.back());
+        freeList_.pop_back();
+        return ret;
+    }
+    else
+        return std::make_unique<ExporterEncodeBuffers>();
+
+}
+
+void Exporter::recycleEncodeBuffers(std::unique_ptr<ExporterEncodeBuffers> toRecycle) const
+{
+    std::lock_guard<std::mutex> guard(freeListMutex_);
+    freeList_.push_back(std::move(toRecycle));
+}
+
+void Exporter::writeInSequence(int64_t iFrame, std::unique_ptr<ExporterEncodeBuffers> buffers) const
+{
+    std::lock_guard<std::mutex> writeQueueGuard(writeQueueMutex_);
+    writeQueue_.push_back(std::pair<int64_t, std::unique_ptr<ExporterEncodeBuffers> >(iFrame, std::move(buffers)));
+
+    bool written;
+    do
+    {
+        written = false;
+        for (auto i = writeQueue_.begin(); i != writeQueue_.end(); ++i)
+        {
+            if (i->first == nextFrameToWrite_)
+            {
+                std::unique_ptr<ExporterEncodeBuffers> buffers = std::move(i->second);
+                writer_->writeFrame(&buffers->output.buffer[0], buffers->output.buffer.size());
+                nextFrameToWrite_++;
+                writeQueue_.erase(i);
+                recycleEncodeBuffers(std::move(buffers));
+                written = true;
+                break;
+            }
+        }
+    } while (written);
+
+    if (nextFrameToWrite_ > nFrames_)
+    {
+        writer_.reset(nullptr);
+    }
 }
 
 csSDK_int32 getPixelFormatSize(const PrFourCC subtype)
--- source/premiere_CC2018/hapencode.hpp
+++ source/premiere_CC2018/hapencode.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <mutex>
+
 #include <PrSDKTypes.h>
 #include <PrSDKWindowSuite.h>
 #include <PrSDKSequenceRenderSuite.h>
@@ -17,6 +19,13 @@
 #include "movie_writer/movie_writer.hpp"
 #include "codec/codec.hpp"
 
+struct ExporterEncodeBuffers
+{
+    EncodeInput input;
+    EncodeScratchpad scratchpad;
+    EncodeOutput output;
+};
+
 class Exporter
 {
 public:
@@ -26,18 +35,26 @@ public:
         int64_t nFrames);
     ~Exporter();
     
+    // thread safe to be called 'on frame rendered'
     void dispatch(int64_t iFrame, const uint8_t* bgra_bottom_left_origin_data, size_t stride) const;
 
 private:
+    std::unique_ptr<ExporterEncodeBuffers> getEncodeBuffers() const;
+    void recycleEncodeBuffers(std::unique_ptr<ExporterEncodeBuffers> toRecycle) const;
+
+    void writeInSequence(int64_t iFrame, std::unique_ptr<ExporterEncodeBuffers> buffers) const;
+
     std::unique_ptr<Codec> codec_;
-    std::unique_ptr<MovieWriter> writer_;
+    mutable std::unique_ptr<MovieWriter> writer_;
     int64_t nFrames_;
 
-    // TODO: these expect dispatch to be called in strict sequence; need to make them pooled for
-    //       multiple simultaneous calls
-    mutable EncodeInput input_;
-    mutable EncodeScratchpad scratchpad_;
-    mutable EncodeOutput output_;
+
+    mutable std::mutex freeListMutex_;
+    mutable std::vector<std::unique_ptr<ExporterEncodeBuffers> > freeList_;
+
+    mutable std::mutex writeQueueMutex_;
+    mutable std::vector<std::pair<int64_t, std::unique_ptr<ExporterEncodeBuffers> > > writeQueue_;
+    mutable int32_t nextFrameToWrite_; // protected with writeQueueMutex_ too
 };
 
 typedef struct ExportSettings
