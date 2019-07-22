ef9d798f757a3233f086a4c8eb254ddd98479d55
Greg Bakker <gbakker@gmail.com>
2019-07-22 14:05:46 +0100

    foundation: begin migration to use foundation-base

--- source/premiere_CC2018/prstring.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <PrSDKStructs.h>
-
-void copyConvertStringLiteralIntoUTF16(const wchar_t* inputString, prUTF16Char* destination);
-void safeStrCpy(char *destStr, int size, const char *srcStr);
