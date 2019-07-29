4269df6177bc38af401ab48f279ad2cfe37bdc7a
Tom Butterworth <bangnoise@gmail.com>
2019-07-29 22:28:52 +0100

    AE, Pr strings to std::string, std::wstring
    Adobe strings need special handling on macOS where they don't trivially convert to wchar_t

--- source/premiere_CC2018/string_conversion.cpp
+++ source/premiere_CC2018/string_conversion.cpp
@@ -2,11 +2,24 @@
 
 #ifndef _WIN64
 #include <codecvt>
+#include <CoreFoundation/CFString.h>
+#include <vector>
 #else
 #include <Windows.h>
 #endif
 
 #include <locale>
+
+static inline int aUTF16CharLength(const A_UTF16Char* inStr)
+{
+    int ret = 0;
+    if (inStr)
+    {
+        for (;*inStr; ++inStr, ++ret)
+        {}
+    }
+    return ret;
+}
 
 std::wstring to_wstring(const std::string& str)
 {
@@ -18,6 +31,11 @@ std::wstring to_wstring(const std::string& str)
 #else
     return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>{}.from_bytes(str);
 #endif
+}
+
+std::wstring to_wstring(const A_UTF16Char *str)
+{
+    return to_wstring(to_string(str));
 }
 
 std::string to_string(const std::wstring& str)
@@ -35,3 +53,33 @@ std::string to_string(const std::wstring& str)
 #endif
 }
 
+std::string to_string(const A_UTF16Char *str)
+{
+#ifdef _WIN64
+    return to_string(std::wstring(reinterpret_cast<wchar_t *>(str)));
+#else
+    CFIndex bytes = aUTF16CharLength(str) * sizeof(A_UTF16Char);
+    CFStringRef input = CFStringCreateWithBytesNoCopy(kCFAllocatorDefault, reinterpret_cast<const UInt8 *>(str), bytes, kCFStringEncodingUTF16, false, kCFAllocatorNull);
+    const char *output = CFStringGetCStringPtr(input, kCFStringEncodingUTF8);
+    if (!output)
+    {
+        CFIndex maximum = CFStringGetMaximumSizeForEncoding(CFStringGetLength(input), kCFStringEncodingUTF8);
+        std::vector<char> buffer(maximum + 1);
+        Boolean result = CFStringGetCString(input, buffer.data(), buffer.size(), kCFStringEncodingUTF8);
+        if (result)
+        {
+            output = buffer.data();
+        }
+    }
+
+    std::string final;
+    if (output)
+    {
+        final = output;
+    }
+
+    CFRelease(input);
+
+    return final;
+#endif
+}
--- source/premiere_CC2018/string_conversion.hpp
+++ source/premiere_CC2018/string_conversion.hpp
@@ -2,6 +2,17 @@
 
 #include <string>
 
+// Used by AE and Pr so re-typedef rather than include headers
+#ifdef _WIN64
+typedef unsigned short  A_u_short;
+#else
+typedef uint16_t        A_u_short;
+#endif
+typedef A_u_short       A_UTF16Char;
+
+
 // nuisance
 std::wstring to_wstring(const std::string& str);
+std::wstring to_wstring(const A_UTF16Char *str);
 std::string to_string(const std::wstring& fromUTF16);
+std::string to_string(const A_UTF16Char *str);
