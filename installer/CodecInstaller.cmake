93f179f4fc527d4656f752bf9a0893e073fa04fa
Tom Butterworth <bangnoise@gmail.com>
2019-10-01 17:33:45 +0100

    Updates for foundation update - presets now handled by foundation

--- installer/CodecInstaller.cmake
+++ installer/CodecInstaller.cmake
@@ -1,20 +1,5 @@
 cmake_minimum_required(VERSION 3.12.0 FATAL_ERROR)
 
-# presets asset files
-
-install(
-    FILES
-        ${CMAKE_CURRENT_SOURCE_DIR}/asset/encoder_preset/ame-12.0/Hap\ Alpha-Only.epr
-        ${CMAKE_CURRENT_SOURCE_DIR}/asset/encoder_preset/ame-12.0/Hap\ Alpha.epr
-        ${CMAKE_CURRENT_SOURCE_DIR}/asset/encoder_preset/ame-12.0/Hap\ Q\ Alpha.epr
-        ${CMAKE_CURRENT_SOURCE_DIR}/asset/encoder_preset/ame-12.0/Hap\ Q.epr
-        ${CMAKE_CURRENT_SOURCE_DIR}/asset/encoder_preset/ame-12.0/Hap.epr
-    DESTINATION
-        "$<IF:$<PLATFORM_ID:Darwin>,Library/Application Support/Hap for Adobe CC/Presets,Presets>"
-    COMPONENT
-        Presets
-)
-
 set(CPACK_PACKAGE_NAME "HapEncoder")
 set(CPACK_PACKAGE_VENDOR "HapCommunity")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "HapEncoder - Hap encoding plugin for Adobe CC 2019")
@@ -25,8 +10,6 @@ set(CPACK_PACKAGE_VERSION_PATCH "0-alpha1")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "HapEncoderPlugin")
 set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_LIST_DIR}/../license.txt)
 set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_LIST_DIR}/ReadMe.txt)
-
-set(CPACK_COMPONENTS_ALL presets plugin)
 
 # NSIS specific settings
 set(CPACK_NSIS_URL_INFO_ABOUT "https://hap.video")
@@ -44,12 +27,3 @@ set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS
 # set(bitmap_path ${CMAKE_CURRENT_LIST_DIR}/../asset/install_image.bmp)
 # STRING(REPLACE "/" "\\" bitmap_path  ${bitmap_path}) 
 # set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP ${bitmap_path})
-
-# codec specific component install paths
-set(Foundation_CODEC_SPECIFIC_COMPONENTS Presets)
-set(CPACK_NSIS_Presets_INSTALL_DIRECTORY "$PROFILE\\\\Documents\\\\Adobe\\\\Adobe\ Media\ Encoder\\\\13.0")
-
-# macOS presets install script
-if (APPLE)
-    set(CPACK_POSTFLIGHT_PRESETS_SCRIPT ${CMAKE_CURRENT_SOURCE_DIR}/installer/postinstall)
-endif (APPLE)
